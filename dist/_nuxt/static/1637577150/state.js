b18bae9f8afad2863660014c332de6a7ac660ff5
Alexandre Bellot <bellot.alexandre@gmail.com>
2021-11-22 11:33:06 +0100

    mise a jours du build

---
+++ dist/_nuxt/static/1637577150/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1637577150",layout:"default",error:b,state:{data:[],inPageItem:c,parallaxInstance:c},serverRendered:true,routePath:a,config:{_app:{basePath:a,assetsPath:"\u002F_nuxt\u002F",cdnURL:b},http:{browserBaseURL:a}}}}("\u002F",null,false));
